71ed94e9e41a1f5aa82509720d61736233630613
mikka <mikkanthrope@gmail.com>
2018-01-10 23:09:07 -0800

    Installed Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'store#index', as: 'store'
-
   resources :items, only: [:show, :index]
   resources :categories, only: [:show, :index]
   resources :users, only: [:show]
